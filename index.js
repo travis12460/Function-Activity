0e26aad4ff1efc52033e27e5a81cb0daaee2cb20
Travis Thomas <travis12460@gmail.com>
2022-12-29 19:22:16 -0500

    First commit. Week 4 function activity

---
+++ index.js
@@ -0,0 +1,5 @@
+function userInfo (name, height, country) {
+    alert ("Hello, my name is " + name + " and my height is " + height + ". I am from " + country);
+}
+
+userInfo("Travis", "5'9", "United States");
